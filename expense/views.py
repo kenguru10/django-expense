c81f9fecc57a2d6fd4aecff47f0b239aad973465
kenguru10 <tinyken20@outlook.com>
2025-12-01 18:05:57 +0800

    feat: Import Sum from django.db.models for aggregate calculations

--- expense/views.py
+++ expense/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.db import transaction
+from django.db.models import Sum
 from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse, HttpResponseBadRequest
 import json
